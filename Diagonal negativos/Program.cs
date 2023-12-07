6bc1a47017c7a78308b4f65c4e2a9d2bbf3068e4
Tony <antony.branquinho@gmail.com>
2023-12-07 17:06:53 -0300

    facil

---
+++ Diagonal negativos/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diagonal_negativos {
+    class Program {
+        static void Main(string[] args) {
+
+            int n, i, j, negativos;
+
+            Console.Write("Qual a ordem da matriz? ");
+            n = int.Parse(Console.ReadLine());
+
+            int[,] matriz = new int[n, n];
+
+            for (i = 0; i < n; i++) {
+                for (j = 0; j < n; j++) {
+                    Console.Write("Elemento [" + j + "," + i + "]: ");
+                    matriz[j, i] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("DIAGONAL PRINCIPAL: ");
+            for (i = 0; i < n; i++) {
+                for (j = 0; j < n; j++) {
+                    if (j == i) {
+                        Console.Write(matriz[i, j] + " ");
+                    }
+                }
+            }
+
+            negativos = 0;
+            for (i = 0; i < n; i++) {
+                for (j = 0; j < n; j++) {
+                    if (matriz[i, j] < 0) {
+                        negativos = negativos + 1;
+                    }
+                }
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
